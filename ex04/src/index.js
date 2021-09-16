0541e90f5bd3d681b3e780579fb45aeea74a344e
sparklymina <ecausevic93@gmail.com>
2021-09-16 13:21:20 -0400

    adding ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // Only change code below this line
+    myDecimal = 8.2
+    myDecimalE = 2.54e-3
+    // Only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;

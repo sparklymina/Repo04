a36634d07f004105767c0e7fa0bb41eea273db11
sparklymina <ecausevic93@gmail.com>
2021-09-16 13:27:52 -0400

    adding ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    // only change code below this line
+    var numOne;
+    var numTwo;
+    myQuotient = numOne / numTwo;
+    // only change code above this line
+    return myQuotient;
+}
+
+console.log(main(5.0, 2.0)); // Change this line
+console.log(main(16.5, 5.5)); // Change this line
+console.log(main(102.0, 25.5)); // Change this line
+module.exports = main;

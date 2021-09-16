98c5a6ebf20b76f1a9ecf57c8780abfda146bf48
sparklymina <ecausevic93@gmail.com>
2021-09-16 13:56:49 -0400

    adding ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Emina";
+    var myLastName = "Pozderac";
+    var myHomeTown = "Istanbul";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;

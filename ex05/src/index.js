eae75120fc192602a8fa5c00da7655917495d065
sparklymina <ecausevic93@gmail.com>
2021-09-16 13:20:15 -0400

    adding ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    // only change code below this line
+    var numOne;
+    var numTwo;
+    product = numOne * numTwo;
+    // only change code above this line
+    return product;
+}
+
+console.log(main(2.5, 2.0)); // Change this line
+console.log(main(5.5, 5.5)); // Change this line
+console.log(main(100.25, 43.20)); // Change this line
+module.exports = main;

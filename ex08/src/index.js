4f874720fa0e45344c484ad9cd0ebcd03d3e54e8
sparklymina <ecausevic93@gmail.com>
2021-09-16 13:39:47 -0400

    adding ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myX = 12 += 3;
+    var myY = 19 += 17;
+    var myZ = 24 -= 20;
+    // only change code above this line
+    return {
+        myX,
+        myY,
+        myZ
+    };
+}
+
+console.log(main());
+module.exports = main;
